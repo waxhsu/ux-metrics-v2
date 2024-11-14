dd7dab508fbc57368c4ec10174166d035454d933
waxhsu <98721786+waxhsu@users.noreply.github.com>
2024-11-14 10:42:54 -0800

    configs removed

--- public/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config. php
-header ( 'Content-Type: application/json');
-$config = [
-    'apiUrl' => getenv('API_URL') ?: 'https://default-api.com',
-    'featureFlag' => getenv( 'FEATURE_FLAG') ?: false,
-1;
-]
-echo json_encode($config)
--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-export async function getConfig() {
-    const response = await fetch('/config.php');
-    if (!response.ok) {
-    throw new Error('Failed to load config');
-    }
-    return await response.json();
-    }
